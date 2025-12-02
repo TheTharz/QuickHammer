147a8ff7e51c7d460423511ba9787b57ca54e678
Tharindu Jayawardhana <tharinduimalkajayawardhana@gmail.com>
2025-12-02 22:18:09 +0530

    update application properties and enhance Swagger configuration for API documentation

--- job-service/src/main/java/com/devnerd/job_service/config/SwaggerConfig.java
+++ job-service/src/main/java/com/devnerd/job_service/config/SwaggerConfig.java
@@ -1,17 +1,31 @@
 package com.devnerd.job_service.config;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI jobServiceOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Job Service API")
-                        .version("1.0")
-                        .description("API documentation for Job Service"));
+                        .description("Job Service for QuickHammer")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("DevNerd")
+                                .email("support@devnerd.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(List.of(
+                        new Server().url("http://localhost:8082").description("Local Server")
+                ));
     }
 }
--- payment-service/src/main/java/com/devnerd/payment_service/config/SwaggerConfig.java
+++ payment-service/src/main/java/com/devnerd/payment_service/config/SwaggerConfig.java
@@ -1,18 +1,32 @@
 package com.devnerd.payment_service.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI paymentServiceOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Auth Service API")
-                        .version("1.0")
-                        .description("API documentation for Auth Service"));
+                        .title("Payment Service API")
+                        .description("Payment Service for QuickHammer")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("DevNerd")
+                                .email("support@devnerd.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(List.of(
+                        new Server().url("http://localhost:8084").description("Local Server")
+                ));
     }
 }
