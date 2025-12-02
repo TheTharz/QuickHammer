a6a752105392c025863ff4ad3a3fc81c5cc62646
Tharindu Jayawardhana <tharinduimalkajayawardhana@gmail.com>
2025-12-02 19:43:26 +0530

    add configuration for Swagger and update application properties

--- api-gateway/src/main/java/com/devnerd/api_gateway/configs/GatewayConfig.java
+++ api-gateway/src/main/java/com/devnerd/api_gateway/configs/GatewayConfig.java
@@ -17,11 +17,41 @@ public class GatewayConfig {
   @Bean
   public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
-            .route("auth-service", r -> r.path("/api/v1/auth/**")
-                .uri("lb://auth-service"))
-            .route("job-service", r -> r.path("/api/v1/jobs/**")
-                .filters(f -> f.filter(jwtAuthFilter))
-                .uri("lb://job-service"))
+                // AUTH SERVICE
+                .route("auth-service", r -> r
+                        .path("/api/v1/auth/**", "/auth-service/**")   // Add swagger route
+                        .filters(f -> f.stripPrefix(1))                // remove "/auth-service" prefix
+                        .uri("lb://auth-service"))
+
+                // JOB SERVICE
+                .route("job-service", r -> r
+                        .path("/api/v1/jobs/**", "/job-service/**")    // Add swagger route
+                        .filters(f -> f.filter(jwtAuthFilter).stripPrefix(1))
+                        .uri("lb://job-service"))
+
+                // USER SERVICE
+                .route("user-service", r -> r
+                        .path("/user-service/**")                      // Add swagger route
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://user-service"))
+
+                // BID SERVICE
+                .route("bid-service", r -> r
+                        .path("/bid-service/**")                       // Add swagger route
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://bid-service"))
+
+                // NOTIFICATION SERVICE
+                .route("notification-service", r -> r
+                        .path("/notification-service/**")              // Add swagger route
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://notification-service"))
+
+                // PAYMENT SERVICE
+                .route("payment-service", r -> r
+                        .path("/payment-service/**")                   // Add swagger route
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://payment-service"))
             .build();
     }
 }
---
+++ api-gateway/src/main/java/com/devnerd/api_gateway/configs/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.devnerd.api_gateway.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain security(ServerHttpSecurity http) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex -> ex
+                        .pathMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+
+                                "/user-service/**",
+                                "/auth-service/**",
+                                "/bid-service/**",
+                                "/job-service/**",
+                                "/notification-service/**",
+                                "/payment-service/**"
+                        ).permitAll()
+
+                        .anyExchange().authenticated()
+                )
+                .build();
+    }
+}
